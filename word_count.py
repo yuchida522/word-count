c2432cfda8739762e0279b5cda5f6fad7f3cfb5e
yuchida522 <yuchida522@gmail.com>
2021-06-29 21:17:03 -0700

    add comments

--- word_count.py
+++ word_count.py
@@ -13,33 +13,39 @@ running instructions
 
 '''
 import re
-def word_count(text, freq_word, integer): 
+def word_count(text, freq_word, n): 
 
+    #setting the common words list into a set so it can be looked up
     freq_word_set = set()
     with open(freq_word) as freq_word_file:
         for line in freq_word_file:
             freq_word_set.add(line.rstrip().lower())
 
+    #where the word count will be stored
     word_count = {}
 
     with open(text) as file:
         for line in file:
+            #I am making all the words lower case, where I am making the assumption that capitalization is not necessary (ex. alice == Alice)
             line_stripped = line.lower().strip()
+            #getting rid of all punctuation, except for apostrophies (ex. won't it's)
             words = re.sub(r'[^\w\d\s\']+', '', line_stripped).split()
             for word in words:
                 if word not in freq_word_set:
                     word_count[word] = word_count.get(word, 0) + 1
     
-    
+    #sort the dictionary 
     sorted_dict = sorted(word_count.items(), key=lambda item:item[1], reverse=True)
 
-    top_words = sorted_dict[:integer]
+    #take only the top n words given from the user input
+    top_words = sorted_dict[:n]
     print("Count", "Word")
     print("===", " ===")
     for word in top_words:
         print(word[1], " ", word[0])
 
 def run_program():
+    ''' takes user inputs '''
     while True:
         content_file = input("Name of file to parse through: ")
         if not content_file:
