db71bbc72665e651d84ac4755971c89e2607750d
yuchida522 <yuchida522@gmail.com>
2021-06-29 21:08:28 -0700

    initial commit

--- word_count.py
+++ word_count.py
@@ -1,3 +1,17 @@
+'''
+running instructions
+
+1. requirements
+    > python3
+    > the two files, text file and common words file, must be placed in the same directory as this file
+2. How to run
+    > clone this repo
+    > Please make sure that the two text files (file to look through, list of common words) are in the same directory as this file
+    > In your terminal, run '$ python3 word_count.py'
+    > Enter the information as prompted
+
+
+'''
 import re
 def word_count(text, freq_word, integer): 
 
@@ -10,9 +24,8 @@ def word_count(text, freq_word, integer):
 
     with open(text) as file:
         for line in file:
-            line_stripped = line.lower().replace(',', '').replace('.', '').strip()
+            line_stripped = line.lower().strip()
             words = re.sub(r'[^\w\d\s\']+', '', line_stripped).split()
-            # print(words)
             for word in words:
                 if word not in freq_word_set:
                     word_count[word] = word_count.get(word, 0) + 1
@@ -21,8 +34,34 @@ def word_count(text, freq_word, integer):
     sorted_dict = sorted(word_count.items(), key=lambda item:item[1], reverse=True)
 
     top_words = sorted_dict[:integer]
-
+    print("Count", "Word")
+    print("===", " ===")
     for word in top_words:
-        print(word[1], word[0])
-    
-word_count('alice_in_wonderland.txt', '1-1000.txt', 10)
+        print(word[1], " ", word[0])
+
+def run_program():
+    while True:
+        content_file = input("Name of file to parse through: ")
+        if not content_file:
+            continue
+        else:
+            break
+    while True:
+        common_words_file = input("Name of the common words file: ")
+        if not common_words_file:
+            continue
+        else:
+            break
+    while True:
+        num = input("How many top common words would you like to see?: ")
+        if not num:
+            continue
+        else: 
+            break
+    try:
+        word_count(content_file, common_words_file, int(num))
+    except:
+        print("You are missing information, please try again")
+
+run_program()
+
